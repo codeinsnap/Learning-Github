6d5780cf962eb519934b82eeb57706121c529c0f
yogesh <yog192325@gmail.com>
2024-04-23 21:40:40 +0530

    testing Commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import { increment, decrement } from './Redux/counterSlice'
 
 function App() {
 
+  console.log('sample', 1 + 1)
+
   return (
     <div style={{ padding: '10px' }}>
       Hello
+      <div>
+        Answer{1 + 1}
+      </div>
     </div>
   );
 }
